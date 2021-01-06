63ba33d6acba9efe25b15d41d8f3052faa002f80
dorbirendorf <dorbiren@gmail.com>
2021-01-06 14:13:34 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express=require('express')
+
+
+const app=express()
+const PORT=process.env.PORT ||5000
+
+
+app.get("/", (req,res)=>res.send('Its alive!'))
+
+
+
+
+
+
+
+
+
+app.listen(PORT,()=> console.log(`Server started on port ${PORT}`))
+
+
+
