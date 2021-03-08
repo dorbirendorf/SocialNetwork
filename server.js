5372ce3916f6cb05a6c552d73a68adea994d925a
dorbirendorf <>
2021-03-08 15:52:27 +0200

    prepared for deployment

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express=require('express')
 require('./config/db')   //runs DB
 const app=express()
+const path=require('path') 
 
 //routers
 const usersRouter=require('./routes/users')
 const postsRouter=require('./routes/posts')
 const profileRouter=require('./routes/profile')
 const authRouter=require('./routes/auth')
+const { appendFile } = require('fs')
 
 //use middelware
 app.use(express.json())
@@ -15,17 +17,21 @@ app.use(express.json())
 const PORT=process.env.PORT ||5000
 
 
-app.get("/", (req,res)=>res.send('Its alive!'))
 app.use('/users',usersRouter)
 app.use('/posts',postsRouter)
 app.use('/auth',authRouter)
 app.use('/profile',profileRouter)
 
+//serve static assest  in production
 
+if (process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
 
-
-
-
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 
 
